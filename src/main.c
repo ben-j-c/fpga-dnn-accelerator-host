fdf2e8cff408e85b3a98b48712f4b69cbb86afcb
Ben <benjamin.j.correia@gmail.com>
2023-02-24 17:27:00 -0500

    calculate bandwidth

--- src/main.c
+++ src/main.c
@@ -24,8 +24,8 @@
 #include "socal/socal.h"
 #include "util.h"
 
-//#define HPS2FPGA_BASE (0xC0000000)
-#define HPS2FPGA_BASE (0xFF200000)
+#define HPS2FPGA_BASE (0xC0000000)
+//#define HPS2FPGA_BASE (0xFF200000)
 #define HPS2FPGA_SPAN (0x00200000)
 
 /*
@@ -35,8 +35,13 @@
 #define HW_REGS_SPAN (0x04000000)
 #define HW_REGS_MASK (HW_REGS_SPAN - 1)
 */
-#define FIFO_IS_FULL(csr)  !!(*(csr + 1) & 0b000001)
-#define FIFO_IS_EMPTY(csr) !!(*(csr + 1) & 0b000010)
+#define FIFO_IS_FULL(csr)    !!(*(csr + 1) & 0b000001)
+#define FIFO_IS_EMPTY(csr)   !!(*(csr + 1) & 0b000010)
+#define FIFO_FILL_LEVEL(csr) (*csr)
+#define FIFO_DEPTH           (128)
+
+#define N_TRANSFERS  (512 * 1024)
+#define VERIFY_VALUE 0
 
 struct prog_state_s
 {
@@ -55,17 +60,41 @@ struct prog_state_s
 	volatile int32_t *pio_status;
 };
 
-static void _state_cleanup(UNUSED struct prog_state_s *state) {}
+static void _state_cleanup(UNUSED struct prog_state_s *state)
+{
+	if (state->virtual_base) {
+		munmap(state->virtual_base, HPS2FPGA_SPAN);
+		state->virtual_base = NULL;
+	}
+	if (state->fd_dev_mem >= 0) {
+again:
+		int res = close(state->fd_dev_mem);
+		if (res <= 0 && errno == EINTR) {
+			goto again;
+		}
+		state->fd_dev_mem = -1;
+	}
+}
 
 static int _recv(struct prog_state_s *s)
 {
+	int32_t data[FIFO_TO_HPS_OUT_CSR_FIFO_DEPTH];
 	int retval = 0;
 	if (!FIFO_IS_EMPTY(s->fifo_to_hps_csr)) {
-		int32_t data = *(s->fifo_to_hps_data);
-		printf("recv=0x%X, ", data);
+		int n  = FIFO_FILL_LEVEL(s->fifo_to_hps_csr);
 		retval = 1;
-		s->recv_count++;
-		ES_NEW_ASRT(data == s->expected, "Assertion failed with %X", data);
+		for (int i = 0; i < n; i++) {
+			data[i] = *(s->fifo_to_hps_data);
+		}
+		s->recv_count += n;
+		if (VERIFY_VALUE) {
+			for (int i = 0; i < n; i++) {
+				ES_NEW_ASRT(data[i] == s->expected,
+				            "Assertion failed with data[%d]=0x%X",
+				            i,
+				            data[i]);
+			}
+		}
 	}
 	return retval;
 }
@@ -74,9 +103,12 @@ static int _send(struct prog_state_s *s)
 {
 	int retval = 0;
 	if (!FIFO_IS_FULL(s->fifo_to_copro_csr)) {
-		*(s->fifo_to_copro_data) = s->value;
-		retval                   = 1;
-		s->send_count++;
+		int n = FIFO_DEPTH - FIFO_FILL_LEVEL(s->fifo_to_copro_csr);
+		for (int i = 0; i < n; i++) {
+			*(s->fifo_to_copro_data) = s->value;
+		}
+		retval = 1;
+		s->send_count += n;
 	}
 	return retval;
 }
@@ -108,29 +140,35 @@ static int _pipeline(UNUSED int argc, UNUSED char **argv)
 	printf("virtual_base=%X\n", (unsigned int) state.virtual_base);
 	printf("fd_dev_mem=%d\n", state.fd_dev_mem);
 
-	while (state.send_count != 512) {
+	int a = *state.pio_status;
+	while (state.send_count != N_TRANSFERS) {
 		int res = _send(&state);
 		int res_recv;
 		ES_FWD_INT_NM(res_recv = _recv(&state));
 		if (res > 0) {
-			printf("send_count=%d, recv_count=%d, pio_status=%d, send_fill=%d, recv_fill=%d\n",
+			/*printf("send_count=%d, recv_count=%d, pio_status=%d, send_fill=%d, recv_fill=%d\n",
 			       state.send_count,
 			       state.recv_count,
 			       *state.pio_status,
 			       *state.fifo_to_copro_csr,
-			       *state.fifo_to_hps_csr);
+			       *state.fifo_to_hps_csr);*/
 		}
 	}
-	printf("Done sending\n");
-	while (state.recv_count != 512) {
+	// printf("Done sending\n");
+	while (state.recv_count != N_TRANSFERS) {
 		int res;
 		ES_FWD_INT_NM(res = _recv(&state));
 		if (res > 0) {
-			printf("recv_count=%d\n", state.recv_count);
+			// printf("recv_count=%d\n", state.recv_count);
 		}
 	}
+	int b = *state.pio_status;
 	printf("Done reading\n");
 	printf("Pipeline finished\n");
+	printf("b-a=%d\n", b - a);
+
+	double bandwidth = (95.0) * (N_TRANSFERS * 4 * 2) / (b - a);
+	printf("bandwidth = %f MBps\n", bandwidth);
 	return 0;
 }
 
